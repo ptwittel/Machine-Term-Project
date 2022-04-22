bd557abf691da6c5256094c8e3037a6327372616
Phillip T Wittel <ptwittel@gmail.com>
2022-04-22 18:19:02 -0500

    changed both train and test df to be caret friendly

--- Data_partition.R
+++ Data_partition.R
@@ -1,9 +1,44 @@
-# this function creates and writes Training and Validation dataFrames, 70:30 split
+# this function creates and writes Training and Validation dataFrames, 70:30 split,
+# this file also cleans the data frame for working with caret
 
-df <- load_data("Raw Data/final_train.csv")
+set.seed(3188055)
+df <- load_data("Raw Data/final_train.csv") 
 
-sample <- sample.int(n = nrow(df), size = floor(0.7 * nrow(df)), replace = FALSE)
 
-df[sample, ] %>% write_csv(., "Clean Data/part_train.csv")
-df[-sample,] %>% write_csv(., "Clean Data/part_validate.csv")
+sample <- caret::createDataPartition(df$default, p = 0.75, list = FALSE)
 
+### Traing Data set
+
+training <- df[sample, ] 
+
+dummieModel <- dummyVars("default ~ .", data = training)
+trainingSetX <- as.data.frame(predict(dummieModel, newdata = training))
+
+scaleModel <- preProcess(trainingSetX, method = "range")      ## MAKE SURE THIS MATCHES THAT
+trainingSetX <- predict(scaleModel, newdata = trainingSetX)
+
+
+trainingSet <- cbind(training$default, trainingSetX)
+names(trainingSet)[1] <- "default"
+
+write_csv(trainingSet, "Clean Data/part_train.csv")
+
+## Testing Data Set
+
+testing <- df[-sample,] 
+
+dummieModel <- dummyVars("default ~ .", data = testing)
+testingSetX <- as.data.frame(predict(dummieModel, newdata = testing))
+
+scaleModel <- preProcess(testingSetX, method = "range")       ## MAKE SURE THAT MATCHES THIS
+testingSetX <- predict(scaleModel, newdata = testingSetX)
+
+testingSet <- cbind(testing$default, testingSetX)
+names(testingSet) <- names(trainingSet)
+
+write_csv(testingSet, "Clean Data/part_validate.csv")
+
+rm(dummieModel, scaleModel, 
+   trainingSetX, training,
+   testingSetX, testing,
+   sample, df)
