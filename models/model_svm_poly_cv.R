e39be3b1432d046ad48fd6e0e4f67861eb0a9419
Phillip T Wittel <ptwittel@gmail.com>
2022-04-23 14:23:21 -0500

    Removed Least Square from the svm_poly_cv

--- models/model_svm_poly_cv.R
+++ models/model_svm_poly_cv.R
@@ -1,16 +1,15 @@
-# Least Square Support Vector Machine
+# Support Vector Machine
 
 twoClassCtrl <- trainControl(method = "repeatedcv",
                              number = 5,
                              repeats = 3,
-                             savePredictions = "final",
                              sampling = "up",
                              verboseIter = TRUE)
 
 svm_poly_cv <- caret::train(default ~ .,
-                       data = trainingSet,
-                       trControl =  twoClassCtrl,
-                       method = "lssvmPoly")
+                            data = trainingSet,
+                            trControl =  twoClassCtrl,
+                            method = "svmPoly")
 
 
 saveRDS(model_log, "Saved Models/model_svm_poly_cv.rds")
