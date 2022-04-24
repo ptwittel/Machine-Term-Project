498045d0622cb383051d93dc2525d7267cdc02a6
Phillip T Wittel <ptwittel@gmail.com>
2022-04-24 00:15:32 -0500

    removed model that were too time intensive

--- Generate Models.R
+++ Generate Models.R
@@ -9,12 +9,14 @@ source("Support Scripts/Data_partition.R")
 ## Pre-Launch procedures
 set.seed(817525159)
 
-trainingSet <- trainingSet %>% sample_frac(0.25) ## For debugging perposes
+trainingSet <- trainingSet ## For debugging purposes
 
-## Run models, saves results in Evironment and ~/Saved Models
+## Run models, saves results in Environment and ~/Saved Models
 (source("models/model_knn_cv_up.R"))
 (source("models/model_log_up.R"))
 (source("models/model_RF_cv_up.R"))
 (source("models/model_xgb_cv_up.R"))
 (source("models/model_svm_cv_up.R"))
+#(source("models/model_svm_poly_cv.R")) HORRABLLY INEFFIENCT COMPUTATIONAL REQUIRMENT!!!
+#(source("models/model_svm_rad_up.R")) #For an additional run for funzes
 
