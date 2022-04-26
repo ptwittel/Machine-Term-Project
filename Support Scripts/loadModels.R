bf7adde3fccc1f34df32d0c4bff1b6ea9ced249d
Phillip T Wittel <ptwittel@gmail.com>
2022-04-26 17:45:33 -0500

    The all of the commits excluding the oversized model.rds files

--- Support Scripts/loadModels.R
+++ Support Scripts/loadModels.R
@@ -5,3 +5,10 @@ model_log_reg <- read_rds("Saved Models/model_log_up.rds")
 model_RF_cv <- read_rds("Saved Models/model_rf_cv_up.rds")
 model_SVM_cv <- read_rds("Saved Models/model_svm_cv.rds")
 model_XGB_cv <- read_rds("Saved Models/model_xgb_cv_up.rds")
+
+
+model_knn <- read_rds("Saved Models/model_knn_AV_cv_up.rds")
+model_log_reg <- read_rds("Saved Models/model_log_AV_up.rds")
+model_RF_cv <- read_rds("Saved Models/model_rf_AV_cv_up.rds")
+model_SVM_cv <- read_rds("Saved Models/model_svm_AV_cv.rds")
+model_XGB_cv <- read_rds("Saved Models/model_xgb_AV_cv_up.rds")
---
+++ models/model_xgb_AP_cv_up.R
@@ -0,0 +1,16 @@
+# Extrem Gradiant Boosting linear up
+
+twoClassCtrl <- trainControl(method = "repeatedcv",
+                             number = 5,
+                             repeats = 3,
+                             savePredictions = "final",
+                             sampling = "up",
+                             verboseIter = TRUE)
+
+xgb_cv_up <- caret::train(default ~ .,
+                       data = trainingSet,
+                       trControl =  twoClassCtrl,
+                       method = "xgbTree")
+
+
+saveRDS(xgb_cv_up, "Saved Models/model_xgb_AV_cv_up.rds")
