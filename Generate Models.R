6b79f5954cba2d38e74f05f69c0510090a858528
Phillip T Wittel <ptwittel@gmail.com>
2022-04-26 18:40:37 -0500

    updated Generated Models.R to have contain all of the steps of the model creating for both regular and added predictors models

--- Generate Models.R
+++ Generate Models.R
@@ -4,12 +4,12 @@ library(tidyverse)
 library(caret)
 
 source("Support Scripts/Global_Func.R")
-source("Support Scripts/Data_preProcessing_alt.R")
+source("Support Scripts/Data_preProcessing.R")
 
 ## Pre-Launch procedures
 set.seed(817525159)
 
-trainingSet <- trainingSet ## For debugging purposes
+# trainingSet <- trainingSet %>% sample_frac(0.01) ## for small model runs and debugging
 
 ## Run models, saves results in Environment and ~/Saved Models
 (source("models/model_knn_cv_up.R"))
@@ -19,6 +19,12 @@ trainingSet <- trainingSet ## For debugging purposes
 (source("models/model_svm_cv_up.R"))
 #(source("models/model_svm_poly_cv.R")) HORRABLLY INEFFIENCT COMPUTATIONAL REQUIRMENT!!!
 #(source("models/model_svm_rad_up.R")) #For an additional run for funzes
+
+
+## Runs model on Added Predictor data, saves results in Environment and ~/Saved Models
+source("Support Scripts/Data_preProcessing_alt.R")
+
+set.seed(817525159)
 
 (source("models/model_knn_AP_cv_up.R"))
 (source("models/model_log_AP_up.R"))
