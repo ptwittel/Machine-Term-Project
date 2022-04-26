4443720a16629778c6ddcb125b4841fe24d72f19
Phillip T Wittel <ptwittel@gmail.com>
2022-04-26 18:48:02 -0500

    Cleaned up this file with comments

--- Generate Models.R
+++ Generate Models.R
@@ -4,12 +4,11 @@ library(tidyverse)
 library(caret)
 
 source("Support Scripts/Global_Func.R")
+
+
+# loading data
 source("Support Scripts/Data_preProcessing.R")
-
-## Pre-Launch procedures
 set.seed(817525159)
-
-# trainingSet <- trainingSet %>% sample_frac(0.01) ## for small model runs and debugging
 
 ## Run models, saves results in Environment and ~/Saved Models
 (source("models/model_knn_cv_up.R"))
@@ -21,11 +20,11 @@ set.seed(817525159)
 #(source("models/model_svm_rad_up.R")) #For an additional run for funzes
 
 
-## Runs model on Added Predictor data, saves results in Environment and ~/Saved Models
+# Loading Added Predictors data
 source("Support Scripts/Data_preProcessing_alt.R")
-
 set.seed(817525159)
 
+## Runs model on Added Predictor data, saves results in Environment and ~/Saved Models
 (source("models/model_knn_AP_cv_up.R"))
 (source("models/model_log_AP_up.R"))
 (source("models/model_RF_AP_cv_up.R"))
